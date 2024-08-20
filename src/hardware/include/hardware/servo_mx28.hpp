b35bc19b963fdacd7e4f10fe5c1add78d6c92aa2
Hernanda <hernandaislearning@gmail.com>
2024-08-20 20:53:49 +0700

    Update sync write and read

--- src/hardware/include/hardware/servo_mx28.hpp
+++ src/hardware/include/hardware/servo_mx28.hpp
@@ -1,78 +1,109 @@
 #ifndef SERVO_MX28_HPP
 #define SERVO_MX28_HPP
 
+//--ROS Header
+#include <ros/package.h>
+#include <ros/ros.h>
+
+//--ROS Messages
+#include <auv_msgs/RPY.msg>
+
+//--Dynamixel Header
+#include <dynamixel_sdk.h>
+
+//--C++ Headers
+#include <stdlib.h>
+
+// Convert degrees to servo position (12-bit resolution)
+#define DEG2POSITION *11.375
+
+// Convert servo position to degrees (12-bit resolution)
+#define POSITION2DEG *0.08791208791
+
 #define ENABLE 1
 #define DISABLE 0
 
-//--Dynamixel Address
+//--Dynamixel Control Table Address for MX-28
 #define ADDR_TORQUE_ENABLE 64
 #define ADDR_GOAL_POSITION 116
 #define ADDR_PRESENT_POSITION 132
 
+//--Dynamixel Bytes Length for MX-28
+#define LEN_GOAL_POSITION 2
+#define LEN_PRESENT_POSITION 2
+
 //--Dynamixel Properties
 #define PROTOCOL_VERSION 2.0 // See which protocol version is being used in the Dynamixel
 #define BAUDRATE 1000000
-#define DEVICENAME "/dev/ttyUSB0"   // Check which port is being used on your controller
-#define MINIMUM_POSITION_LIMIT 0    // Refer to the Minimum Position Limit of product eManual
-#define MAXIMUM_POSITION_LIMIT 4095 // Refer to the Maximum Position Limit of product eManual
-#define MOVING_STATUS_THRESHOLD 10  // Resolution of 1 degree is estimated to be 11,375
+#define PATH "/dev/ttyUSB0" // Check which port is being used on your controller
 
 //--Dynamixel ID
 #define ID_YAW 1
 #define ID_PITCH 2
 
-//--Servo Limitation
-#define YAW_MINIMUM 1137.5
-#define YAW_MAXIMUM 2957.5
-#define PITCH_MINIMUM 2047.5
-#define PITCH_MAXIMUM 2730
-
-//--ROS Header
-#include <ros/ros.h>
-#include <ros/package.h>
-
-//--Dynamixel Header
-#include <hardware/dynamixel_sdk.h>
+//--Dynamixel Limit Values
+#define MOVING_STATUS_THRESHOLD 10 // Resolution of 1 degree is estimated to be 11,375
+#define YAW_MINIMUM 100 DEG2POSITION // Minimum position for yaw servo
+#define YAW_MAXIMUM 260 DEG2POSITION // Maximum position for yaw servo
+#define PITCH_MINIMUM 180 DEG2POSITION // Minimum position for pitch servo
+#define PITCH_MAXIMUM 240 DEG2POSITION // Maximum position for pitch servo
 
 //--Timer
 ros::Timer servo_routine;
 
 //--Subscriber
+ros::Subscriber rpy_sub;
 
 //--Publisher
+ros::Publisher rpy_pub;
 
-//--Prototypes
-void servo_routine_callback(const ros::TimerEvent &event);
-
-//--Global Variables
-
-class _ServoMX28
-{
+class _ServoMX28 {
 private:
-    dynamixel::PortHandler *portHandler;
-    dynamixel::PacketHandler *packetHandler;
+    dynamixel::PortHandler* portHandler;
+    dynamixel::PacketHandler* packetHandler;
+    dynamixel::GroupSyncWrite* groupSyncWrite;
+    dynamixel::GroupSyncRead* groupSyncRead;
+
+    int dxl_comm_result; // Communication result
+    bool dxl_addparam_result = false; // addParam result
+    bool dxl_getdata_result = false; // GetParam result
+    uint8_t dxl_error; // DYNAMIXEL error
 
     // Private constructor to prevent instantiation
-    _ServoMX28() : portHandler(nullptr), packetHandler(nullptr), index(0), dxl_comm_result{0, 0}, dxl_error{0, 0}, dxl_present_position{0, 0}, dxl_goal_position{0, 0} {}
+    _ServoMX28()
+        : portHandler(nullptr)
+        , packetHandler(nullptr)
+        , groupSyncWrite(nullptr)
+        , groupSyncRead(nullptr)
+        , dxl_comm_result(0)
+        , dxl_addparam_result(false)
+        , dxl_getdata_result(false)
+        , dxl_error(0)
+    {
+    }
 
     // Delete copy constructor and assignment operator
-    _ServoMX28(const _ServoMX28 &) = delete;
-    _ServoMX28 &operator=(const _ServoMX28 &) = delete;
+    _ServoMX28(const _ServoMX28&) = delete;
+    _ServoMX28& operator=(const _ServoMX28&) = delete;
+
+    uint8_t param_yaw_goal_position[2]; // Goal position parameter written to the yaw servo
+    uint8_t param_pitch_goal_position[2]; // Goal position parameter written to the pitch servo
+    int16_t yaw_present_position = 0; // Present position
+    int16_t pitch_present_position = 0; // Present position
 
 public:
-    int index;
-    int dxl_comm_result[2];          // Communication result
-    uint8_t dxl_error[2];            // DYNAMIXEL error
-    int32_t dxl_present_position[2]; // Read 4 byte Position data
-    uint16_t dxl_goal_position[2][2];
+    uint16_t yaw_goal_position = 180 DEG2POSITION; // Goal position
+    u_int16_t pitch_goal_position = 210 DEG2POSITION; // Goal position
 
     // Static method to get the singleton instance
-    static _ServoMX28 &getInstance()
+    static _ServoMX28& getInstance()
     {
         static _ServoMX28 instance;
         return instance;
     }
-    void servo_routine_callback(const ros::TimerEvent &event);
+
+    void servo_routine_callback(const ros::TimerEvent& event);
+    void rpy_callback(const auv_msgs::RPY::ConstPtr& msg);
     void servo_mx28_init();
     int set_torque(uint8_t _id, uint8_t _flag);
     int disable_torque(uint8_t _id, uint8_t _flag);
--- src/hardware/src/servo_mx28.cpp
+++ src/hardware/src/servo_mx28.cpp
@@ -1,6 +1,6 @@
 #include "hardware/servo_mx28.hpp"
 
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
     ros::init(argc, argv, "servo_mx28");
     ros::NodeHandle nh;
@@ -9,50 +9,99 @@ int main(int argc, char **argv)
     //--Initialize Servo
     _ServoMX28::getInstance().servo_mx28_init();
 
+    //--Subscriber
+    rpy_sub = nh.subscribe("/master/servo_goal_pose", 1, &_ServoMX28::rpy_callback);
+
+    //--Publisher
+    rpy_pub = nh.advertise<auv_msgs::RPY>("/hardware/servo_current_pose", 1);
+
     //--Timer
-    servo_routine = nh.createTimer(ros::Duration(1.1), &_ServoMX28::servo_routine_callback, &_ServoMX28::getInstance());
+    servo_routine = nh.createTimer(ros::Duration(0.01), &_ServoMX28::servo_routine_callback, &_ServoMX28::getInstance());
     spinner.spin();
     return 0;
 }
 
-void _ServoMX28::servo_routine_callback(const ros::TimerEvent &event)
+void _ServoMX28::rpy_callback(const auv_msgs::RPY::ConstPtr& msg)
 {
-    // Write Goal Position
-    packetHandler->write4ByteTxRx(portHandler, ID_YAW, ADDR_GOAL_POSITION, dxl_goal_position[0][index], &dxl_error[0]);
-    packetHandler->write4ByteTxRx(portHandler, ID_PITCH, ADDR_GOAL_POSITION, dxl_goal_position[1][index], &dxl_error[1]);
+    yaw_goal_position = msg->yaw * DEG2POSITION;
+    pitch_goal_position = msg->pitch * DEG2POSITION;
+}
 
-    // Read Present Position
-    // do
-    // {
-    dxl_comm_result[0] = packetHandler->read4ByteTxRx(portHandler, ID_YAW, ADDR_PRESENT_POSITION, (uint32_t *)&dxl_present_position[0], &dxl_error[0]);
-    printf("[ID:%03d] Goal Position:%03d  Present Position:%03d\n", ID_YAW, dxl_goal_position[0][index], dxl_present_position[0]);
-    // } while ((abs(dxl_goal_position[0][index] - dxl_present_position[0]) > MOVING_STATUS_THRESHOLD));
+void _ServoMX28::servo_routine_callback(const ros::TimerEvent& event)
+{
+    param_yaw_goal_position[0] = DXL_LOBYTE(yaw_goal_position);
+    param_yaw_goal_position[1] = DXL_HIBYTE(yaw_goal_position);
+    param_pitch_goal_position[0] = DXL_LOBYTE(pitch_goal_position);
+    param_pitch_goal_position[1] = DXL_HIBYTE(pitch_goal_position);
 
-    // do
-    // {
-    dxl_comm_result[1] = packetHandler->read4ByteTxRx(portHandler, ID_PITCH, ADDR_PRESENT_POSITION, (uint32_t *)&dxl_present_position[1], &dxl_error[1]);
-    printf("[ID:%03d] Goal Position:%03d  Present Position:%03d\n", ID_PITCH, dxl_goal_position[1][index], dxl_present_position[1]);
-    // } while ((abs(dxl_goal_position[1][index] - dxl_present_position[1]) > MOVING_STATUS_THRESHOLD));
+    // Add Dynamixel goal position value to the Syncwrite storage
+    dxl_addparam_result = groupSyncWrite->addParam(ID_YAW, param_yaw_goal_position);
+    if (dxl_addparam_result != true) {
+        fprintf(stderr, "[ID:%03d] groupSyncWrite addparam failed", YAW_ID);
+        return 0;
+    }
 
-    // Switch the Goal Position
-    index = (index == 0) ? 1 : 0;
+    dxl_addparam_result = groupSyncWrite->addParam(ID_PITCH, param_pitch_goal_position);
+    if (dxl_addparam_result != true) {
+        fprintf(stderr, "[ID:%03d] groupSyncWrite addparam failed", PITCH_ID);
+        return 0;
+    }
+
+    // Syncwrite goal position
+    dxl_comm_result = groupSyncWrite->txPacket();
+    if (dxl_comm_result != COMM_SUCCESS) {
+        fprintf(stderr, "%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+    }
+
+    // Clear syncwrite parameter storage
+    groupSyncWrite->clearParam();
+
+    // Syncread present position
+    dxl_comm_result = groupSyncRead->txRxPacket();
+    if (dxl_comm_result != COMM_SUCCESS) {
+        fprintf(stderr, "%s\n", packetHandler->getTxRxResult(dxl_comm_result));
+    }
+
+    // Check if groupSyncRead data of Dynamixel present position is available
+    dxl_getdata_result = groupSyncRead->isAvailable(ID_YAW, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION);
+    if (dxl_getdata_result != true) {
+        fprintf(stderr, "[ID:%03d] groupSyncRead getdata failed", YAW_ID);
+        return 0;
+    }
+
+    dxl_getdata_result = groupSyncRead->isAvailable(ID_PITCH, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION);
+    if (dxl_getdata_result != true) {
+        fprintf(stderr, "[ID:%03d] groupSyncRead getdata failed", PITCH_ID);
+        return 0;
+    }
+
+    // Get present position value
+    yaw_present_position = groupSyncRead->getData(ID_YAW, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION);
+    pitch_present_position = groupSyncRead->getData(ID_PITCH, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION);
+
+    auv_msgs::RPY rpy_msg;
+    rpy_msg.yaw = yaw_present_position * POSITION2DEG;
+    rpy_msg.pitch = pitch_present_position * POSITION2DEG;
+
+    if (rpy_msg.yaw > 180)
+        rpy_msg.yaw -= 360;
+    else if (rpy_msg.yaw < -180)
+        rpy_msg.yaw += 360;
+
+    if (rpy_msg.pitch > 180)
+        rpy_msg.pitch -= 360;
+    else if (rpy_msg.pitch < -180)
+        rpy_msg.pitch += 360;
+
+    rpy_pub.publish(rpy_msg);
 }
 
 void _ServoMX28::servo_mx28_init()
 {
-    portHandler = dynamixel::PortHandler::getPortHandler(DEVICENAME);
+    portHandler = dynamixel::PortHandler::getPortHandler(PATH);
     packetHandler = dynamixel::PacketHandler::getPacketHandler(PROTOCOL_VERSION);
-    index = 0;
-    dxl_comm_result[0] = COMM_TX_FAIL;       // Communication result
-    dxl_comm_result[1] = COMM_TX_FAIL;       // Communication result
-    dxl_error[0] = 0;                        // DYNAMIXEL error
-    dxl_error[1] = 0;                        // DYNAMIXEL error
-    dxl_goal_position[0][0] = YAW_MINIMUM;   // Goal position
-    dxl_goal_position[0][1] = YAW_MAXIMUM;   // Goal position
-    dxl_goal_position[1][0] = PITCH_MINIMUM; // Goal position
-    dxl_goal_position[1][1] = PITCH_MAXIMUM; // Goal position
-    dxl_present_position[0] = 0;             // Read 4 byte Position data
-    dxl_present_position[1] = 0;             // Read 4 byte Position data
+    groupSyncWrite = new dynamixel::GroupSyncWrite(portHandler, packetHandler, ADDR_GOAL_POSITION, LEN_GOAL_POSITION);
+    groupSyncRead = new dynamixel::GroupSyncRead(portHandler, packetHandler, ADDR_PRESENT_POSITION, LEN_PRESENT_POSITION);
 
     // Open port
     (portHandler->openPort()) ? printf("Succeeded to open the port!\n") : printf("Failed to open the port!\n");
@@ -63,6 +112,12 @@ void _ServoMX28::servo_mx28_init()
     // Enable DYNAMIXEL Torque
     dxl_comm_result[0] = set_torque(ID_YAW, ENABLE);
     dxl_comm_result[1] = set_torque(ID_PITCH, ENABLE);
+
+    // Add parameter storage for Dynamixel present position
+    dxl_addparam_result = groupSyncRead->addParam(ID_YAW);
+    (dxl_addparam_result) ? printf("Succeeded to add parameter for present position!\n") : printf("Failed to add parameter for present position!\n");
+    dxl_addparam_result = groupSyncRead->addParam(ID_PITCH);
+    (dxl_addparam_result) ? printf("Succeeded to add parameter for present position!\n") : printf("Failed to add parameter for present position!\n");
 }
 
 int _ServoMX28::set_torque(uint8_t _id, uint8_t _flag)
